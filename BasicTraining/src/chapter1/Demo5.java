b2e26d13bc7e7f55afd16ae5af411af866ee70a6
Jun <wangzhen_tju@126.com>
2021-05-30 11:44:13 +0800

    private修饰方法、变量不能在该类外引用

---
+++ BasicTraining/src/chapter1/Demo5.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Demo5 {
+
+    private String a = "abc";
+
+    private void print(){
+        System.out.println("hello");
+    }
+}
---
+++ BasicTraining/src/chapter1/Demo6.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Demo6 {
+
+    public static void main(String[] args) {
+        Demo5 demo = new Demo5();
+        System.out.println(demo.a);
+        demo.print();
+    }
+}
