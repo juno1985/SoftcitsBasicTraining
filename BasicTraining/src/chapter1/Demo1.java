7f23ae75ea51d3f232f953708291bdd6f72c85d7
Jun <wangzhen_tju@126.com>
2021-05-30 10:21:14 +0800

    完成了Demo1

---
+++ BasicTraining/src/chapter1/Demo1.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("fdafa");
+        }
+
+        System.out.println("fdafa");
+    }
+}
