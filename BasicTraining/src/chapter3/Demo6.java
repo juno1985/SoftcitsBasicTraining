d4defb7b03b49b220770d239d8b6fb547c00e752
Jun <wangzhen_tju@126.com>
2021-06-20 15:39:08 +0800

    Java Loops I
    https://www.hackerrank.com/challenges/java-loops-i/problem

---
+++ BasicTraining/src/chapter3/Demo6.java
@@ -0,0 +1,22 @@
+package chapter3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Demo6 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+
+
+    }
+}
